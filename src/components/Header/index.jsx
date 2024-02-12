f752aedbd2aa1ad784b0bd2c903093826fa1b073
gautam123-sachin <sachingautam6239@gmail.com>
2024-02-12 05:59:25 +0530

    update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
